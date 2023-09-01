78450710c900c41afbbdf02bae1d37ae9542c8be
Bala <bala01.mobility@gmail.com>
2023-09-01 18:59:51 +0530

    vehicle_are

--- resources/views/admin/vehicle_fare/_fare_list.blade.php
+++ resources/views/admin/vehicle_fare/_fare_list.blade.php
@@ -1,111 +1,104 @@
-<div class="box-body no-padding">
-    <div class="table-responsive">
-      <table class="table table-hover">
-            <thead> 
+<ul class="nav nav-tabs" id="zoneTabs" role="tablist">
+    @php
+    $zoneNames = [];
+    $zoneIndex = 0;
+    @endphp
+    @foreach ($results as $key => $result)
+    @php
+    $zoneName = $result->zoneType->zone->name;
+    if (!in_array($zoneName, $zoneNames)) {
+        $zoneNames[] = $zoneName;
+    }
+    @endphp
+    @endforeach
+    @foreach ($zoneNames as $zoneName)
+    <li class="nav-item" role="presentation">
+        <a class="nav-link @if ($zoneIndex === 0) active @endif" id="zone-tab-{{ $zoneIndex }}" data-toggle="tab" href="#zone-{{ $zoneIndex }}" role="tab" aria-controls="zone-{{ $zoneIndex }}" aria-selected="true">{{ $zoneName }}</a>
+    </li>
+    @php
+    $zoneIndex++;
+    @endphp
+    @endforeach
+</ul>
 
-                    <tr>
-<th> @lang('view_pages.s_no')
-<span style="float: right;">
-</span>
-</th>
-<th> @lang('view_pages.zone_name')
-<span style="float: right;">
-</span>
-</th><th> @lang('view_pages.transport_type')
-<span style="float: right;">
-</span>
-</th><th> @lang('view_pages.vehicle_type')
-<span style="float: right;">
-</span>
-</th><th> @lang('view_pages.price_type')
-<span style="float: right;">
-</span>
-</th><th> @lang('view_pages.status')
-<span style="float: right;">
-</span>
-</th><th> @lang('view_pages.action')
-<span style="float: right;">
-</span>
-</th>
+<div class="tab-content" id="zoneTabsContent">
+    @php
+    $zoneIndex = 0;
+    @endphp
+    @foreach ($zoneNames as $zoneName)
+    <div class="tab-pane fade @if ($zoneIndex === 0) show active @endif" id="zone-{{ $zoneIndex }}" role="tabpanel">
+        <table class="table table-hover">
+            <!-- Table Header -->
+            <thead>
+                <!-- Header Rows -->
+                <tr>
+                    <!-- Header Columns -->
+                    <th>@lang('view_pages.s_no')</th>
+                    <th>@lang('view_pages.transport_type')</th>
+                    <th>@lang('view_pages.vehicle_type')</th>
+                    <th>@lang('view_pages.price_type')</th>
+                    <th>@lang('view_pages.status')</th>
+                    <th>@lang('view_pages.action')</th>
                 </tr>
-                </thead>
-                <tbody>
-                    @php  $i= $results->firstItem(); @endphp
-                    @forelse ($results as $key => $result)
-
-                            <td>{{ $i++ }}</td>
-                            <td>{{ ($result->zoneType->zone->name) }}</td>
-                            <td>{{ ($result->zoneType->transport_type) }}</td>
-                            <td>{{ $result->zoneType->vehicleType->name }}
-                            @if ($result->zoneType->zone->default_vehicle_type == $result->zoneType->vehicleType->id)
-                            <button class="btn btn-warning btn-sm">Default</button>
-					        @endif
-                            @if ($result->zoneType->zone->default_vehicle_type_for_delivery == $result->zoneType->vehicleType->id)
-                            <button class="btn btn-warning btn-sm">Default</button>
-					        @endif
-                            </td>
-                            <td>
-                                @if ($result->price_type == 1)
-                                    <span class="btn btn-success btn-sm">{{ __('view_pages.ride_now') }}</span>
-                                @else
-                                    <span class="btn btn-danger btn-sm">{{ __('view_pages.ride_later') }}</span>
-                                @endif
-                            </td>
-                                @if ($result->zoneType->active)
-                            <td><button class="btn btn-success btn-sm">@lang('view_pages.active')</button></td>
-                                @else
-                            <td><button class="btn btn-danger btn-sm">@lang('view_pages.inactive')</button></td>
-                                @endif
-                           <td>
-                            <button type="button" class="btn btn-info btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">@lang('view_pages.action')
-                            </button>
-                            <div class="dropdown-menu w-48 ">
-                                <a class="dropdown-item" href="{{url('vehicle_fare/edit', $result->id)}}">
-                                    <i class="fa fa-pencil"></i>@lang('view_pages.edit')
-                                </a>
-                                <a class="dropdown-item" href="{{url('vehicle_fare/rental_package/index', $result->zoneType->id)}}">
-                                    <i class="fa fa-plus"></i>@lang('view_pages.assign_rental_package')
-                                </a>
-                                @if($result->zoneType->transport_type == 'taxi')
-                                    @if ($result->active == 1 && $result->zoneType->zone->default_vehicle_type != $result->zoneType->vehicleType->id)
-                                    <a class="dropdown-item" href="{{url('vehicle_fare/set/default',$result->id)}}"><i class="fa fa-dot-circle-o"></i>@lang('view_pages.set_as_default')</a>
-                                    @endif  
-                                @else  
-                                    @if ($result->active == 1 && $result->zoneType->zone->default_vehicle_type_for_delivery !=  $result->zoneType->vehicleType->id)
-                                    <a class="dropdown-item" href="{{url('vehicle_fare/set/default',$result->id)}}"><i class="fa fa-dot-circle-o"></i>@lang('view_pages.set_as_default')</a>
-                                    @endif 
-                                @endif       
-                                @if($result->zoneType->active)
-                                    <a class="dropdown-item" href="{{url('vehicle_fare/toggle_status', $result->id)}}">
-                                    <i class="fa fa-dot-circle-o"></i>@lang('view_pages.inactive')</a>
-                                @else
-                                    <a class="dropdown-item" href="{{url('vehicle_fare/toggle_status', $result->id)}}">
-                                    <i class="fa fa-dot-circle-o"></i>@lang('view_pages.active')</a>
-                                @endif  
-                                <a class="dropdown-item sweet-delete" href="#" data-url="{{url('vehicle_fare/delete',$result->id)}}">
-                                 <i class="fa fa-trash-o"></i>@lang('view_pages.delete')</a>                                                              
-                            </div>
+            </thead>
+            <!-- Table Body -->
+            <tbody>
+                @php
+                $i = 1;
+                @endphp
+                @foreach ($results as $key => $result)
+                @if ($result->zoneType->zone->name === $zoneName)
+                <!-- Data Rows -->
+                <tr>
+                    <td>{{ $i++ }}</td>
+                    <td>{{ ($result->zoneType->transport_type) }}</td>
+                    <td>{{ $result->zoneType->vehicleType->name }}</td>
+                    <td>
+                        @if ($result->price_type == 1)
+                        <span class="btn btn-success btn-sm">{{ __('view_pages.ride_now') }}</span>
+                        @else
+                        <span class="btn btn-danger btn-sm">{{ __('view_pages.ride_later') }}</span>
+                        @endif
+                    </td>
+                    <td>
+                        @if ($result->zoneType->active)
+                        <button class="btn btn-success btn-sm">@lang('view_pages.active')</button>
+                        @else
+                        <button class="btn btn-danger btn-sm">@lang('view_pages.inactive')</button>
+                        @endif
+                    </td>
+                    <td>
+                        <!-- Dropdown for Actions -->
+                        <button type="button" class="btn btn-info btn-sm dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">@lang('view_pages.action')</button>
+                        <div class="dropdown-menu w-48">
+                            <a class="dropdown-item" href="{{url('vehicle_fare/edit', $result->id)}}">
+                                <i class="fa fa-pencil"></i>@lang('view_pages.edit')
+                            </a>
+                            <a class="dropdown-item" href="{{url('vehicle_fare/rental_package/index', $result->zoneType->id)}}">
+                                <i class="fa fa-plus"></i>@lang('view_pages.assign_rental_package')
+                            </a>
+                            @if ($result->active == 1 && $result->zoneType->zone->default_vehicle_type != $result->zoneType->vehicleType->id)
+                            <a class="dropdown-item" href="{{url('vehicle_fare/set/default',$result->id)}}"><i class="fa fa-dot-circle-o"></i>@lang('view_pages.set_as_default')</a>
+                            @endif
+                            @if($result->zoneType->active)
+                            <a class="dropdown-item" href="{{url('vehicle_fare/toggle_status', $result->id)}}">
+                                <i class="fa fa-dot-circle-o"></i>@lang('view_pages.inactive')</a>
+                            @else
+                            <a class="dropdown-item" href="{{url('vehicle_fare/toggle_status', $result->id)}}">
+                                <i class="fa fa-dot-circle-o"></i>@lang('view_pages.active')</a>
+                            @endif
+                            <a class="dropdown-item sweet-delete" href="#" data-url="{{url('vehicle_fare/delete',$result->id)}}">
+                                <i class="fa fa-trash-o"></i>@lang('view_pages.delete')</a>
+                        </div>
+                    </td>
                 </tr>
-                    @empty
-                        <tr>
-                            <td colspan="6">
-                                <div id="no_data" class="lead no-data text-center">
-                                    <img src="{{ asset('assets/img/dark-data.svg') }}">
-                                    <h4 class="text-center">@lang('view_pages.no_data_found')</h4>
-                                </div>
-                            </td>
-                        </tr>
-                    @endforelse
-                </tbody>
-            </table>
-        </div>
+                @endif
+                @endforeach
+            </tbody>
+        </table>
     </div>
-</div>
-
-<div class="intro-y g-col-12 d-flex flex-wrap flex-sm-row flex-sm-nowrap align-items-center">
-    <nav class="w-full w-sm-auto me-sm-auto">
-        <ul class="pagination">
-            {{ $results->links('pagination::bootstrap-4') }}
-        </ul>
-    </nav>
+    @php
+    $zoneIndex++;
+    @endphp
+    @endforeach
 </div>
--- resources/views/track-request.blade.php
+++ resources/views/track-request.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Request - Tagxi</title>
-    <link rel="shortcut icon" href="{{ fav_icon() ?? asset('assets/images/favicon.png')}}">
+    <link rel="shortcut icon" href="{{ fav_icon() ?? asset('assets/images/favicon.ico') }}">
     <link rel="stylesheet" href="{!! asset('css/track-request.css') !!}">
 </head>
 
@@ -29,16 +29,216 @@
         font-weight: 800;
         font-size: large;
     }
+
+/*timeline*/
+@media (min-width:992px) {
+    .page-container {
+        max-width: 1140px;
+        margin: 0 auto
+    }
+
+    .page-sidenav {
+        display: block !important
+    }
+}
+
+.padding {
+    padding: 2rem
+}
+
+.w-32 {
+    width: 32px !important;
+    height: 32px !important;
+    font-size: .85em
+}
+
+.tl-item .avatar {
+    z-index: 2
+}
+
+.circle {
+    border-radius: 500px
+}
+
+.gd-warning {
+    color: #fff;
+    border: none;
+    background: #f4c414 linear-gradient(45deg, #f4c414, #f45414)
+}
+
+.timeline {
+    position: relative;
+    border-color: rgba(160, 175, 185, .15);
+    padding: 0;
+    margin: 0
+}
+
+.p-4 {
+    padding: 1.5rem !important
+}
+
+.block,
+.card {
+    background: #fff;
+    border-width: 0;
+    border-radius: .25rem;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, .05);
+    margin-bottom: 1.5rem
+}
+
+.mb-4,
+.my-4 {
+    margin-bottom: 1.5rem !important
+}
+
+.tl-item {
+    border-radius: 3px;
+    position: relative;
+    display: -ms-flexbox;
+    display: flex
+}
+
+.tl-item>* {
+    padding: 10px
+}
+
+.tl-item .avatar {
+    z-index: 2
+}
+
+.tl-item:last-child .tl-dot:after {
+    display: none
+}
+
+.tl-item.active .tl-dot:before {
+    border-color: #448bff;
+    box-shadow: 0 0 0 4px rgba(68, 139, 255, .2)
+}
+
+.tl-item:last-child .tl-dot:after {
+    display: none
+}
+
+.tl-item.active .tl-dot:before {
+    border-color: #34b807;
+    box-shadow: 0 0 0 4px rgba(68, 139, 255, .2)
+}
+
+.tl-dot {
+    position: relative;
+    border-color: rgba(160, 175, 185, .15)
+}
+
+.tl-dot:after,
+.tl-dot:before {
+    content: '';
+    position: absolute;
+    border-color: inherit;
+    border-width: 2px;
+    border-style: solid;
+    border-radius: 50%;
+    width: 10px;
+    height: 10px;
+    top: 15px;
+    left: 50%;
+    transform: translateX(-50%)
+}
+
+.tl-dot:after {
+    width: 0;
+    height: auto;
+    top: 25px;
+    bottom: -15px;
+    border-right-width: 0;
+    border-top-width: 0;
+    border-bottom-width: 0;
+    border-radius: 0
+}
+
+tl-item.active .tl-dot:before {
+    border-color:#34b807;
+    box-shadow: 0 0 0 4px rgba(68, 139, 255, .2)
+}
+
+.tl-dot {
+    position: relative;
+    border-color: rgba(160, 175, 185, .15)
+}
+
+.tl-dot:after,
+.tl-dot:before {
+    content: '';
+    position: absolute;
+    border-color: inherit;
+    border-width: 2px;
+    border-style: solid;
+    border-radius: 50%;
+    width: 10px;
+    height: 10px;
+    top: 15px;
+    left: 50%;
+    transform: translateX(-50%)
+}
+
+.tl-dot:after {
+    width: 0;
+    height: auto;
+    top: 25px;
+    bottom: -15px;
+    border-right-width: 0;
+    border-top-width: 0;
+    border-bottom-width: 0;
+    border-radius: 0
+}
+
+.tl-content p:last-child {
+    margin-bottom: 0
+}
+
+.tl-date {
+    font-size: .85em;
+    margin-top: 2px;
+    min-width: 100px;
+    max-width: 100px
+}
+
+.avatar {
+    position: relative;
+    line-height: 1;
+    border-radius: 500px;
+    white-space: nowrap;
+    font-weight: 700;
+    border-radius: 100%;
+    display: -ms-flexbox;
+    display: flex;
+    -ms-flex-pack: center;
+    justify-content: center;
+    -ms-flex-align: center;
+    align-items: center;
+    -ms-flex-negative: 0;
+    flex-shrink: 0;
+    border-radius: 500px;
+    box-shadow: 0 5px 10px 0 rgba(50, 50, 50, .15)
+}
+
+.b-warning {
+    border-color: #b1b1b1!important;
+}
+
+.b-primary {
+    border-color: #f63f3f!important;
+}
+
+.b-danger {
+    border-color: #f54394!important;
+}
+/*timeline end*/
 </style>
 
-<body class="bg-gray-400">
+<body class="bg-white-400">
 
     @if($request->is_completed || $request->is_cancelled)
-    <!-- <div class="w-screen">
-        <img src="{{ asset('map/logo.png') }}" alt="" class="w-full h-16 object-cover object-bottom">    
-    </div> -->
-
-    <div class="flex justify-center h-screen">
+    <div id="completed-design" class="flex justify-center h-screen">
         <div class="flex-column text-black font-bold rounded-lg mt-40">
             <div class="flex justify-center">
                 <img src="{{ asset('map/tick.png') }}" alt="" class="rounded w-10 h-10">
@@ -53,52 +253,29 @@
             <div class="m-1 p-2 rounded shadow-lg">
                 <div class="mx-auto flex justify-center items-center mb-5">
                     <!-- <div class="w-full text-center"> -->
-                    <strong class="text-blue-900">{{ $request->request_number }} -</strong>
-                    @if ($request->is_completed)
-                    <p class="text-md text-black font-bold ml-3 trip_status">@lang('view_pages.trip_completed')</p>
-                    @elseif ($request->is_cancelled)
-                    <p class="text-md text-black font-bold ml-3 trip_status">@lang('view_pages.trip_cancelled')</p>
-                    @elseif ($request->is_trip_start)
-                    <p class="text-md text-black font-bold ml-3 trip_status">@lang('view_pages.on_going_ride')</p>
-                    @else
-                    <p class="text-md text-black font-bold ml-3 trip_status">@lang('view_pages.driver_is_on_the_way')</p>
-                    @endif
+                    <strong class="text-blue-900">{{ $request->request_number }} - </strong>
+                    <p class="text-md text-black font-bold ml-3 trip_status"></p>
+
 
                     <!-- </div> -->
                 </div>
                 <hr>
-                <div class="flex justify-between items-center m-3">
-                    <div class="flex-column">
-                        <p class="font-sans leading-relaxed text-lg text-gray-700 text-center font-extrabold">@lang('view_pages.pickup')</p>
-                        <p class="font-sans leading-relaxed text-lg text-gray-700 text-center font-extrabold">@lang('view_pages.drop')</p>
-                    </div>
-                    <div class="flex-column">
-                        <p class="font-serif leading-relaxed text-lg text-black text-center font-hairline">{{ str_limit($request->requestPlace->pick_address,30) }}</p>
-                        <p class="font-serif leading-relaxed text-lg text-black text-center font-hairline">{{ str_limit($request->requestPlace->drop_address,30) }}</p>
-                    </div>
-                </div>
-            </div>
 
-            <!-- Map -->
+      <!-- Map -->
             <div class="lg:mt-10 mt-6">
                 <div id="map"></div>
             </div>
 
+                    <p class="text-md text-black font-bold ml-3 ride_otp"></p>
+                
+            </div>
+
+
+
             <!-- Driver Details -->
             <div class="bg-white rounded shadow-lg m-5 p-3 lg:mt-10">
                 <div class="flex justify-between">
-                    <div class="flex items-center">
-                        <img src="{{ $request->zoneType->vehicleType->icon_image ?? 'https://cdn0.iconfinder.com/data/icons/isometric-city-basic-transport/480/car-front-02-128.png' }}" alt="" class="rounded shadow-lg" width="50" height="50">
-                        <p class="ml-2 text-gray-900">{{ $request->zoneType->vehicleType->name }}</p>
-                    </div>
-                    <div class="flex">
-                        <p>{{ $request->driverDetail->car_number }}</p>
-                    </div>
-                </div>
 
-                <hr>
-
-                <div class="flex justify-between m-2">
                     <div class="flex items-center">
                         <img src="{{ $request->driverDetail->user->profile_pic ?? 'https://cdn4.iconfinder.com/data/icons/rcons-user/32/child_boy-128.png' }}" alt="" class="rounded-full h-12 w-12 flex items-center justify-center" width="50" height="50">
 
@@ -108,23 +285,85 @@
                             <p class="flex flex-row">
                                 @for($i = 0; $i < $request->driverDetail->user->rating; $i++)
                                     <img src="https://cdn2.iconfinder.com/data/icons/ios-7-icons/50/star-128.png" alt="" class="h-4 w-4 items-center justify-center bg-yellow">
-                                    @endfor
+                                @endfor
                             </p>
 
                         </div>
+                        <div class="flex items-center ml-3 text-center">
+                        <p style="color:red;">
+                            @lang('view_pages.estimated_price')
+                        {{$request->requested_currency_symbol}}  {{ $request->request_eta_amount}}
+                        </p>
                     </div>
 
-                    <div class="flex">
-                        <a href="tel:{{ $request->driverDetail->mobile }}">
-                            <img src="https://www.iconfinder.com/data/icons/font-awesome/1792/phone-square-128.png" alt="" class="rounded" width="50" height="50">
-                        </a>
+                    </div>
+
+                    
+                    <div class="flex items-center">
+                    <ul class="box-controls pull-right">
+                    <li>
+                    <div class="flex" style="border:1px solid black;padding: 5px;margin-left: 5px;">
+                        <p>{{ $request->driverDetail->car_number }}</p>
+                    </div>
+                    </li>
+                     <li>
+                        <p class="ml-2 text-gray-900">{{ $request->driverDetail->carMake->name }}</p>
+                        <p class="ml-2 text-gray-900">{{ $request->driverDetail->carModel->name }}</p>
+                    </li>
+                        </div>
+                    </ul>
+
+                    </div>
+                </div>
+
+                <hr>
+
+                <div class="flex justify-between m-2" style="margin-left: 35%;padding: 20px;">
+                    <div class="flex"  >
+                        <p >
+                            <a href="tel:{{ $request->driverDetail->mobile }}" style="display: flex;align-items: center;
+                            ">
+                                <img  src="{{asset('assets/img/Phone.svg')}}" style="width:30px">
+                               @lang('view_pages.call_driver')
+                          </a>
+                        </p> 
                     </div>
                 </div>
 
             </div>
+  <!-- pickup & drop address -->
+
+  <div class="page-content page-container" id="page-content">
+    <div class="padding">
+        <div class="row">
+            
+            <div class="col-lg-6">
+                <p>Location Details</p>
+                <div class="timeline p-4 block mb-4">
+                    <div class="tl-item active">
+                        <div class="tl-dot b-warning"></div>
+                        <div class="tl-content">
+                            <div class="">Pickup</div>
+                            <div class="tl-date text-muted mt-1">{{ str_limit($request->requestPlace->pick_address,30) }}</div>
+                        </div>
+                    </div>
+                    <div class="tl-item">
+                        <div class="tl-dot b-primary"></div>
+                        <div class="tl-content">
+                            <div class="">Drop</div>
+                            <div class="tl-date text-muted mt-1">{{ str_limit($request->requestPlace->drop_address,30) }}</div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            
+        
         </div>
-        <div class="hidden lg:flex lg:w-1/2">
-            <img src="{{ asset('map/logo1.png') }}" alt="" class="rounded w-full object-cover">
+    </div>
+</div>
+
+      <!-- pickup & drop address end-->
         </div>
     </div>
 
@@ -146,8 +385,8 @@
         var driverLat, driverLng, bearing;
 
         // Your web app's Firebase configuration
-        var firebaseConfig = {
-                apiKey: "{{get_settings('firebase-api-key')}}",
+    var firebaseConfig = {
+    apiKey: "{{get_settings('firebase-api-key')}}",
     authDomain: "{{get_settings('firebase-auth-domain')}}",
     databaseURL: "{{get_settings('firebase-db-url')}}",
     projectId: "{{get_settings('firebase-project-id')}}",
@@ -161,23 +400,75 @@
         firebase.initializeApp(firebaseConfig);
         firebase.analytics();
 
+var tripStatusElement = document.createElement('p'); // Create a new <p> element
+tripStatusElement.classList.add('text-md', 'text-black', 'font-bold', 'ml-3', 'trip_status'); // Add classes to the element
+
+var rideOtpElement = document.createElement('p'); // Create a new <p> element for ride OTP
+rideOtpElement.classList.add('text-md', 'text-black', 'font-bold', 'ml-3', 'ride_otp'); // Add classes to the element
+
+var tripRef = firebase.database().ref('requests/' + requestId);
+
+tripRef.on('value', function(snapshot) {
+    var data = snapshot.val();
+    console.log(data);
+
+    var tripStatusText = '';
+
+    if (data.is_completed == true) {
+        tripStatusText = '@lang("view_pages.trip_completed")';
+        location.reload();
+    } else if (data.is_cancelled == true) {
+        tripStatusText = '@lang("view_pages.trip_cancelled")';
+    } else if (data.trip_start == "1") {
+        tripStatusText = '@lang("view_pages.trip_started")';
+
+    } else if (data.trip_arrived == "1") {
+        tripStatusText = '@lang("view_pages.driver_arrived")';
+
+        // Display ride OTP and set its value
+        rideOtpElement.textContent = '@lang("view_pages.ride_otp") : ' + '{{ $request->ride_otp }}';
+
+        // Replace the existing trip status element
+        var existingTripStatusElement = document.querySelector('.trip_status');
+        if (existingTripStatusElement) {
+            existingTripStatusElement.replaceWith(tripStatusElement);
+        }
+        
+        // Append the ride OTP element
+        var rideOtpContainer = document.querySelector('.ride_otp');
+        if (rideOtpContainer) {
+            rideOtpContainer.replaceWith(rideOtpElement);
+        }
+    } else {
+        tripStatusText = '@lang("view_pages.driver_is_on_the_way")';
+
+    }
+
+        tripStatusElement.textContent = tripStatusText;
+
+        var existingTripStatusElement = document.querySelector('.trip_status'); 
+        if (existingTripStatusElement) {
+            existingTripStatusElement.replaceWith(tripStatusElement); 
+        }
+    });
+
 
         var tripRef = firebase.database().ref('requests/' + requestId);
 
-        tripRef.on('value', async function(snapshot) {
+        // tripRef.on('value', async function(snapshot) {
 
-            var data = snapshot.val();
+        //     var data = snapshot.val();
 
-            // console.log(data);
+        //     console.log(data);
 
-            driverLat = data.lat;
-            driverLng = data.lng;
-            bearing = data.bearing;
+        //     driverLat = data.lat;
+        //     driverLng = data.lng;
+        //     bearing = data.bearing;
 
-            await loadCarInMap(driverLat, driverLng, bearing, carimage);
+        //     await loadCarInMap(driverLat, driverLng, bearing, carimage);
 
-            // await rotateMarker(bearing);
-        });
+        //     // await rotateMarker(bearing);
+        // });
 
 
         var area1, area2, icon1, icon2;
