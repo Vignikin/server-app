a536e02be2cd4285150a99bd6a0c5d362af7a695
Bala <bala01.mobility@gmail.com>
2023-10-06 17:40:15 +0530

    new

--- app/Transformers/Owner/OwnerProfileTransformer.php
+++ app/Transformers/Owner/OwnerProfileTransformer.php
@@ -116,6 +116,8 @@ class OwnerProfileTransformer extends Transformer
                 
             }
 
+            $params['show_wallet_feature_on_mobile_app'] =  get_settings('show_wallet_feature_on_mobile_app');
+
             $params['low_balance'] = $low_balance;
 
 
