040eca6f1b97e5ea37bcfe0e89f56ec1a0bb88c3
Bala <bala01.mobility@gmail.com>
2023-10-10 18:38:43 +0530

    new

--- resources/views/admin/drivers/_driver-ratings.blade.php
+++ resources/views/admin/drivers/_driver-ratings.blade.php
@@ -75,6 +75,10 @@
 
                                     </tbody>
                                 </table>
-
+             <div class="text-right">
+                <span  style="float:right">
+                {{$results->links()}}
+                </span>
+            </div>
+        </div>
     </div>
-</div>
