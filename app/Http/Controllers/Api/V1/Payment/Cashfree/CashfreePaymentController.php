fc650fcc478d9d024613ba54218131319ba9b755
Dilip <tagyourtaxi@gmail.com>
2023-11-04 19:06:33 +0530

    local changes

--- app/Http/Controllers/Api/V1/Payment/Cashfree/CashfreePaymentController.php
+++ app/Http/Controllers/Api/V1/Payment/Cashfree/CashfreePaymentController.php
@@ -57,6 +57,7 @@ class CashfreePaymentController extends ApiController
         $headers = [
             'x-client-id:'.get_settings(Settings::CASH_FREE_TEST_APP_ID),
             'x-client-secret:'.get_settings(Settings::CASH_FREE_SECRET_KEY),
+            'x-api-version:"2022-09-01"',
             'Content-Type:application/json'
             ];
 
