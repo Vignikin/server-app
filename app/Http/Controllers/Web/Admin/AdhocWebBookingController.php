a7c38f7ea15a8d9c3d459acf4beffdc88acc2803
Dilip <tagyourtaxi@gmail.com>
2023-12-22 14:38:45 +0530

    locla changes

--- app/Http/Controllers/Web/Admin/AdhocWebBookingController.php
+++ app/Http/Controllers/Web/Admin/AdhocWebBookingController.php
@@ -33,6 +33,12 @@ use App\Transformers\Requests\PackagesTransformer;
 use App\Models\Master\PackageType; 
 use Illuminate\Support\Facades\Session;
 use Auth;
+use App\Base\Constants\Masters\UserType;
+use App\Base\Constants\Masters\WalletRemarks;
+use App\Base\Constants\Masters\zoneRideType;
+use App\Base\Constants\Masters\PaymentType;
+use App\Models\Admin\CancellationReason;
+use Illuminate\Support\Facades\Artisan;
 
 /**
  * @group User-trips-apis
@@ -216,11 +222,10 @@ class AdhocWebBookingController extends BaseController
             $request_params['offerred_ride_fare']=$request->offerred_ride_fare;
         }
 
-        if($request->input('is_later')==1&&$request->has('trip_start_time')){
+        if($request->input('is_later')&&$request->has('trip_start_time')){
 
             $request_params['trip_start_time'] = Carbon::parse($request->trip_start_time, $user_detail->timezone)->setTimezone('UTC')->toDateTimeString();
-            
-            $request_params['is_later']=1; 
+    
         }
         if($request->has('rental_package_id') && $request->rental_package_id){
 
@@ -319,6 +324,141 @@ class AdhocWebBookingController extends BaseController
         $result = fractal($type, new PackagesTransformer);
 
         return $this->respondSuccess($result);         
+
+    }
+
+
+
+    /**
+     * Adhoc Cancel Booking
+     * @bodyParam request_id uuid required id of request
+     * @bodyParam reason string optional reason provided by user
+     * @bodyParam custom_reason string optional custom reason provided by 
+     * 
+     * */
+    public function cancelRide(ValidatorRequest $request){
+
+        // Validate Request id
+        $request->validate([
+            'request_id'=>'required|exists:requests,id',
+            'reason'=>'sometimes|required',
+            'custom_reason'=>'sometimes|required|min:2|max:100',
+        ]);
+
+        $user = auth()->user();
+        $request_detail = $user->requestDetail()->where('id', $request->request_id)->first();
+        // Throw an exception if the user is not authorised for this request
+        if (!$request_detail) {
+            $this->throwAuthorizationException();
+        }
+        $request_detail->update([
+            'is_cancelled'=>true,
+            'reason'=>$request->reason,
+            'custom_reason'=>$request->custom_reason,
+            'cancel_method'=>UserType::USER,
+            'cancelled_at'=>date('Y-m-d H:i:s')
+        ]);
+
+        $request_detail->fresh();
+        /**
+        * Apply Cancellation Fee
+        */
+        $charge_applicable = false;
+
+        if ($request->custom_reason) {
+            $charge_applicable = true;
+        }
+        if ($request->reason) {
+            $reason = CancellationReason::find($request->reason);
+            if($reason){
+
+            if ($reason->payment_type=='free') {
+                $charge_applicable=false;
+            } else {
+                $charge_applicable=true;
+            }
+
+            }else{
+
+                $charge_applicable = false;
+            }
+            
+        }
+
+        /**
+         * get prices from zone type
+         */
+
+            $ride_type = zoneRideType::RIDENOW;
+
+
+        if ($charge_applicable) {
+            $zone_type_price = $request_detail->zoneType->zoneTypePrice()->where('price_type', $ride_type)->first();
+
+            $cancellation_fee = $zone_type_price->cancellation_fee;
+            if ($request_detail->payment_opt==PaymentType::WALLET) {
+                $requested_user = $request_detail->userDetail;
+                $user_wallet = $requested_user->userWallet;
+                $user_wallet->amount_spent += $cancellation_fee;
+                $user_wallet->amount_balance -= $cancellation_fee;
+                $user_wallet->save();
+                // Add the history
+                $requested_user->userWalletHistory()->create([
+                    'amount'=>$cancellation_fee,
+                    'transaction_id'=>$request_detail->id,
+                    'remarks'=>WalletRemarks::CANCELLATION_FEE,
+                    'request_id'=>$request_detail->id,
+                    'is_credit'=>false]);
+                $request_detail->requestCancellationFee()->create(['user_id'=>$request_detail->user_id,'is_paid'=>true,'cancellation_fee'=>$cancellation_fee,'paid_request_id'=>$request_detail->id]);
+            } else {
+                $request_detail->requestCancellationFee()->create(['user_id'=>$request_detail->user_id,'is_paid'=>false,'cancellation_fee'=>$cancellation_fee]);
+            }
+        }
+
+        // Available the driver who belongs to the request
+        $request_driver = $request_detail->driverDetail;
+        if ($request_driver) {
+            $driver = $request_driver;
+        } else {
+            $request_meta_driver = $request_detail->requestMeta()->where('active', true)->first();
+            if($request_meta_driver){
+            $driver = $request_meta_driver->driver;
+
+            }else{
+                $driver=null;
+            }
+        }
+
+        // Delete Meta Driver From Firebase
+            $this->database->getReference('request-meta/'.$request_detail->id)->remove();
+
+        
+        if ($driver) {
+
+            // $this->database->getReference('request-meta/'.$request_detail.'/'.$driver->id)->remove();
+
+            $driver->available = true;
+            $driver->save();
+            $driver->fresh();
+            // Notify the driver that the user is cancelled the trip request
+            $notifiable_driver = $driver->user;
+            $request_result =  fractal($request_detail, new TripRequestTransformer)->parseIncludes('userDetail');
+
+            $push_request_detail = $request_result->toJson();
+            $title = trans('push_notifications.trip_cancelled_by_user_title');
+            $body = trans('push_notifications.trip_cancelled_by_user_body');
+           
+            dispatch(new SendPushNotification($notifiable_driver,$title,$body));;
+        }
+        // Delete meta records
+        // RequestMeta::where('request_id', $request_detail->id)->delete();
+        
+        $request_detail->requestMeta()->delete();
+
+        Artisan::call('assign_drivers:for_regular_rides');
+         
+        return $this->respondSuccess();
+
 
     }
 }
